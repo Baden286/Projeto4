cb8dc583387341576997bf513efddab97af24508
pablo miguel de oliveira <pablomigue@gmail.com>
2018-11-08 07:51:58 -0200

    adicionando mais estados para o Quiz.
    adicionando mais estados para o Quiz.

--- src/java/br/com/fatec/quiz/Quiz.java
+++ src/java/br/com/fatec/quiz/Quiz.java
@@ -1,7 +1,7 @@
 package br.com.fatec.quiz;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 public class Quiz {
     private static ArrayList<Question> test;
 
@@ -10,15 +10,20 @@ public class Quiz {
         if (test==null){
         
             test = new ArrayList<>();
-            test.add(new Question ("Sa"))
+            test.add(new Question ("Sao Paulo", "Sao Paulo", new String[]{"santos","Sao Paulo","campinas"}));
+            test.add(new Question ("Rio de Janeiro", "Rio de Janeiro", new String[]{"Niteroi","Rio de Janeiro","Parati"}));
+            test.add(new Question("Rio Grande do Sul","Porto Alegre", new String[]{"Caxias","Porto Alegre","Pelotas"}));
+            test.add(new Question ("Parana","Curitiba", new String[]{"Pamana","curitiba","Corinthias"}));
+            test.add(new Question ("Goias","Goiania", new String[]{"Goiania","Santos","Vila Bela"}));
             test.add(new Question ("Alemanha","Berlin",new String[]{"Asmara","Berlin","Mônaco"}));
             test.add(new Question ("Angola","Luanda",new String[]{"Luanda","Manila","Malabo"}));
             test.add(new Question ("Austrália","Camberra",new String[]{"Camberra","Otava","Jamena"}));
             test.add(new Question ("Chile","Santiago",new String[]{"Pequin","Santiago","Seul"}));
         
-        Collections
+        //Misturar a lista    
+        Collections.shuffle(test);
         
-        }
+       }
         return test;
     }
 }
